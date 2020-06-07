8b1dabf42aa1279d3774d43b6e36e5c3bbd7fbe3
Shadan Abdulkarim <shadanabdulkarim94@gmail.com>
2020-06-07 19:20:03 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -13,3 +13,30 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+let globalVar=''
+function loadAll(){
+  return fetch('http://localhost:3000/toys')
+  .then(resp => resp.json())
+  .then(json => {
+    console.log(json);
+    globalVar=json;
+    CardsFunc()
+    })
+}
+
+function CardsFunc(){
+  const divCollection=document.getElementById('toy-collection')
+ globalVar.forEach(item =>{
+   const div=document.createElement('div')
+   div.innerHTML=`
+   <h2>${item.Title}</h2>
+   <img src=${item.source}>
+   <p>${item.likes}</p>
+      <button class="like-btn">Like <3</button>
+   `
+   div.classList.add('card')
+   divCollection.appendChild(div)
+ }) 
+}
+
+loadAll()
