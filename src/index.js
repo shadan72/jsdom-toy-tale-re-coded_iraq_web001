b5fba5c9cba411db7b33b6c568512871a6640d1f
Shadan Abdulkarim <shadanabdulkarim94@gmail.com>
2020-06-07 19:09:27 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -14,10 +14,7 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 });
 
-function loadAll(){
+
   return fetch('http://localhost:3000/toys')
   .then(resp => resp.json())
   .then(json => console.log(json))
-}
-
-loadAll()
